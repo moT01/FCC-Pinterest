96fc080addb3f9e275b6821feb1fe9c0ece21a5f
ahmadabdolsaheb <ahmad.abdolsaheb@gmail.com>
2017-12-12 16:49:05 +0300

    instant loading photo

--- client/src/components/common/TextFieldGroup.js
+++ client/src/components/common/TextFieldGroup.js
@@ -2,14 +2,14 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextFieldGroup = ({field, value, label, error, type, onChange, checkUserExists}) => {
+const TextFieldGroup = ({field, value, label, error, type, onChange, checkPhotoExists}) => {
   return(
     <div className={classnames("form-group", { 'has-error': error })}>
       <label className="control-label">{label}</label>
       <input
         value={value}
         onChange={onChange}
-        onBlur={checkUserExists}
+        onBlur={checkPhotoExists}
         type={type}
         name={field}
         className="form-control"
@@ -26,7 +26,7 @@ TextFieldGroup.propTypes = {
   error: PropTypes.string,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  checkUserExists: PropTypes.func
+  checkPhotoExists: PropTypes.func
 }
 
 TextFieldGroup.defaultProps = {
--- client/src/components/createPost/CreatePostForm.js
+++ client/src/components/createPost/CreatePostForm.js
@@ -3,6 +3,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import { createPost } from '../../actions/postsActions';
 import { connect } from 'react-redux';
 import { addFlashMessage } from '../../actions/flashMessages.js';
+import checkImage from 'image-check';
 //import PropTypes from 'prop-types'
 
 class CreatePostForm extends React.Component {
@@ -10,19 +11,52 @@ class CreatePostForm extends React.Component {
     super(props);
     this.state = {
       imageURL: '',
+      defaultImage: 'https://goo.gl/yNUY3G',
       userID: this.props.id,
       username: this.props.username,
-      isLoading: false
+      isLoading: false,
+      sendable:false
     };
-    
+
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e){
     this.setState({[e.target.name]: e.target.value});
+    if(this.checkForImage(e.target.value) && this.isURL(e.target.value)){
+      this.validate(e.target.value);
+    }else{
+      this.setState({defaultImage: "https://goo.gl/yNUY3G", sendable: false});
+    }
   }
 
+  //checks for valid image
+  validate(URL, state){
+    checkImage(URL).then((data) => {
+        this.setState({defaultImage: URL, sendable: true});
+    }).catch((err) => {
+      //error handeling
+        this.setState({defaultImage: "https://goo.gl/yNUY3G", sendable: false});
+    });
+  }
+
+  //checks if it is an image
+  checkForImage(url) {
+    return(url.match(/\.(jpeg|jpg|gif|png)$/) != null);
+  }
+
+  //checks if input is a url
+  isURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?'+ // port
+    '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
+    '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return pattern.test(str);
+  }
   onSubmit(e) {
   	 console.log('submit create post form');
     e.preventDefault();
@@ -34,13 +68,12 @@ class CreatePostForm extends React.Component {
     });
     console.log('end of submit create post form');
   }
-
   render() {
-    const { imageURL, isLoading } = this.state;
-
+    const { imageURL, isLoading, sendable } = this.state;
     return (
       <form onSubmit={this.onSubmit}>
         <h1> Create a Post... </h1>
+        <img src={this.state.defaultImage} className="img-thumbnail" alt="Cinque Terre" width="304" height="236"/>
         <TextFieldGroup
           field="imageURL"
           label="Image URL:"
@@ -48,7 +81,7 @@ class CreatePostForm extends React.Component {
           value={imageURL}
           onChange={this.onChange}
           />
-        <button type="submit" disabled={isLoading} className="btn btn-primary">Add</button>
+        <button type="submit" disabled={isLoading || !sendable} className="btn btn-primary">Add</button>
       </form>
     );
   }
