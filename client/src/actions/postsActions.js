76a279129ca52060da036e0f817517b6d2827e23
moT01 <TMondloch01@gmail.com>
2017-12-15 22:13:39 -0600

    move function

--- client/src/actions/postsActions.js
+++ client/src/actions/postsActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_MY_POSTS,LOAD_POSTS, CREATE_POST, DELETE_POST } from './types';
+import { GET_MY_POSTS, LOAD_POSTS, CREATE_POST, DELETE_POST } from './types';
 
 export function loadPosts(postsToDisplay) {
   return {
@@ -12,7 +12,6 @@ export function getAllPosts(data) {
   console.log('getAllPosts action');
   return dispatch => {
     return axios.get('/api/posts/allPosts', data).then(res => {
-      //console.log('getAllPosts action.then');
       const allPosts = res.data;
       console.log("allPosts" + res.data);
       dispatch(loadPosts(allPosts));
@@ -31,7 +30,6 @@ export function getMyPosts(id) {
   console.log('getUserPosts action');
   return dispatch => {
     return axios.patch('/api/posts/getUserPosts', {id}).then(res => {
-      //console.log('getUserPosts action.then');
       const myPosts = res.data;
       console.log("myPosts" + myPosts);
       dispatch(loadMyPosts(myPosts));
@@ -46,33 +44,10 @@ export function AddNewPost(post) {
   };
 }
 
-export function postToDelete(postID) {
-  return {
-    type: DELETE_POST,
-    postID
-  };
-}
-
-
-
-export function deletePost(postID, postOwnerID, authenticatedUserID) {
-  console.log('deletePost action');
-
-  return dispatch => {
-    return axios.patch('/api/posts/deletePost', { postID, postOwnerID, authenticatedUserID }).then(res => {
-      console.log('deletePost action.then => dispatch');
-      const postID = res.data[0];
-      dispatch(postToDelete(postID));
-    });
-  }
-}
-
-
 export function createPost(data) {
   console.log('createPost action');
   return dispatch => {
     return axios.post('/api/posts/createPost', data).then(res => {
-      console.log('createPost action.then');
       console.log(res.data.post);
       const newPost = (res.data.post);
 
@@ -81,7 +56,22 @@ export function createPost(data) {
   }
 }
 
+export function postToDelete(postID) {
+  return {
+    type: DELETE_POST,
+    postID
+  };
+}
 
+export function deletePost(postID, postOwnerID, authenticatedUserID) {
+  console.log('deletePost action');
+  return dispatch => {
+    return axios.patch('/api/posts/deletePost', { postID, postOwnerID, authenticatedUserID }).then(res => {
+      const postID = res.data[0];
+      dispatch(postToDelete(postID));
+    });
+  }
+}
 
 export function getMyPins(username) {
   console.log('getMyPins action');
