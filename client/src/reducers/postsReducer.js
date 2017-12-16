612dd378eaf105ea439637de5bc00567ebd64594
moT01 <TMondloch01@gmail.com>
2017-12-15 22:08:33 -0600

    remove whitespace (compile warning)

--- client/src/reducers/postsReducer.js
+++ client/src/reducers/postsReducer.js
@@ -11,12 +11,12 @@ export default (state = initialState, action = {}) => {
   switch(action.type) {
     case LOAD_POSTS:
     console.log(action);
-      return { ... state,
+      return { ...state,
         postsToDisplay: action.postsToDisplay
       }
     case CREATE_POST:
     console.log(action);
-      return { ... state,
+      return { ...state,
         postsToDisplay: [
           ...state.postsToDisplay,
           action.post
@@ -24,12 +24,12 @@ export default (state = initialState, action = {}) => {
       }
     case GET_MY_POSTS:
     console.log(action);
-      return { ... state,
+      return { ...state,
       myPosts: action.posts
       }
     case DELETE_POST:
     console.log(action);
-      return { ... state,
+      return { ...state,
         postsToDisplay: state.postsToDisplay.filter(post => post._id !== action.postID),
         myPosts: state.myPosts.filter(post => post._id !== action.postID)
       }
