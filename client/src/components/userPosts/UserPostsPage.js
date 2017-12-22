35730d66d7adb6fc586de6821c9a8efae6f2ea0b
ahmadabdolsaheb <ahmad.abdolsaheb@gmail.com>
2017-12-22 17:41:13 +0300

    added masonry to userposts and removed postsForm

--- client/src/components/userPosts/UserPostsPage.js
+++ client/src/components/userPosts/UserPostsPage.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import UserPostsForm from './UserPostsForm';
+import { deletePost, getUserPosts } from '../../actions/postsActions';
+import { connect } from 'react-redux';
+import { addFlashMessage } from '../../actions/flashMessages.js';
+import Post from '../common/Post';
+import StackGrid from "react-stack-grid";
 
 class UserPostsPage extends React.Component {
+  componentWillMount() {
+      this.props.getUserPosts(this.props.match.params.userId);
+  }
   render() {
-    console.log(this.props);
-    console.log("hello");
     return(
-      <div className="row">
-        <div className="col-md-4 col-md-offset-4">
-          <UserPostsForm userId={this.props.match.params.userId}/>
-        </div>
-      </div>
+      <StackGrid columnWidth={150}>
+        {this.props.userPosts.map((post, index) =>
+        <Post key={index} post={post}/>
+        )}
+      </StackGrid>
     );
   }
 }
-//
-export default UserPostsPage;
+
+function mapStateToProps(state) {
+    return {
+      message: state.postsReducer.message,
+      userPosts: state.postsReducer.userPosts,
+    }
+}
+
+export default connect(mapStateToProps, {addFlashMessage, getUserPosts, deletePost})(UserPostsPage);
